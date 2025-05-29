14ed704074ca49020908d28882eb243737ee2ecb
Samuel <samymateru1999@gmail.com>
2025-05-29 12:05:34 +0300

    adding dashboards

--- AuditNew/Internal/reports/routes.py
+++ AuditNew/Internal/reports/routes.py
@@ -10,10 +10,10 @@ router = APIRouter(prefix="/reports")
 async def fetch_detailed_issue_reports(
         company_module_id: str,
         db=Depends(get_async_db_connection),
-        #user: CurrentUser = Depends(get_current_user)
+        user: CurrentUser = Depends(get_current_user)
 ):
-    #if user.status_code != 200:
-        #raise HTTPException(status_code=user.status_code, detail=user.description)
+    if user.status_code != 200:
+        raise HTTPException(status_code=user.status_code, detail=user.description)
     try:
         main_report_data = await get_main_reports(connection=db, company_module_id=company_module_id)
         if main_report_data is None:
